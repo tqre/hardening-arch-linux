caaf4a382ea19c269b730beb2a9f45b896f9d6c8
tqre <tkuure@gmail.com>
2020-04-22 11:25:40 +0300

    Return of cpu generated random number support

--- install/arch_install_upcloud.sh
+++ install/arch_install_upcloud.sh
@@ -96,7 +96,7 @@ systemctl enable sshd
 # GRUB installation
 grub-install --target=i386-pc /dev/vda
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
-sed -i '/LINUX_DEF/c\GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"' /etc/default/grub
+sed -i '/LINUX_DEF/c\GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet random.trust_cpu=on"' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 EOF
