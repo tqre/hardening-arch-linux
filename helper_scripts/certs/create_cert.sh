92bcc6c014ee0344e07806b7105f73d9b5314a29
tqre <tkuure@gmail.com>
2020-06-06 20:01:56 +0300

    Helper script to create new TLS certificate

---
+++ helper_scripts/certs/create_cert.sh
@@ -0,0 +1,11 @@
+# Check certificate expiry & create new SSL cert for saltmaster
+#!/bin/bash
+
+cat /etc/ssl/private/saltmaster.crt | openssl x509 -noout -enddate
+
+openssl req -x509 -nodes -newkey rsa:4096 -days 365 -keyout /etc/nginx/ssl/saltmaster.key -out /etc/nginx/ssl/saltmaster.crt
+sudo cp -v /etc/nginx/ssl/saltmaster.crt /srv/salt/managed_files/etc/ssl/private/saltmaster.crt
+
+# Don't forget to:
+sudo systemctl restart nginx
+
