0baedd1f627e8fdcca2c0e411f881d1fb21ac41b
tqre <tkuure@gmail.com>
2020-06-06 22:26:01 +0300

    Need sudo rights to create cert in the place

--- helper_scripts/certs/create_cert.sh
+++ helper_scripts/certs/create_cert.sh
@@ -3,7 +3,7 @@
 
 cat /etc/ssl/private/saltmaster.crt | openssl x509 -noout -enddate
 
-openssl req -x509 -nodes -newkey rsa:4096 -days 365 -keyout /etc/nginx/ssl/saltmaster.key -out /etc/nginx/ssl/saltmaster.crt
+sudo openssl req -x509 -nodes -newkey rsa:4096 -days 365 -keyout /etc/nginx/ssl/saltmaster.key -out /etc/nginx/ssl/saltmaster.crt
 sudo cp -v /etc/nginx/ssl/saltmaster.crt /srv/salt/managed_files/etc/ssl/private/saltmaster.crt
 
 # Don't forget to:
